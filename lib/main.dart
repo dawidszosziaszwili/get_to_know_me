4831b61bc895f0e703483842a10b75f9248d2473
Dawid Szosziaszwili <dawidszwili7@gmail.com>
2023-05-24 08:10:41 +0200

    Navigate to second and first page

--- lib/main.dart
+++ lib/main.dart
@@ -16,46 +16,121 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('GetToKnowMe'),
-          backgroundColor: Color.fromARGB(255, 119, 179, 141),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetToKnowMe'),
+        backgroundColor: Color.fromARGB(255, 119, 179, 141),
+      ),
+      backgroundColor: Color.fromARGB(255, 182, 238, 203),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage('images/image.png'),
+              radius: 155,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              'Dawid Szosziaszwili',
+              style: GoogleFonts.lobster(
+                fontSize: 45,
+                color: const Color.fromARGB(255, 64, 64, 69),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Flutter Developer',
+              style: GoogleFonts.dancingScript(
+                fontSize: 40,
+                color: const Color.fromARGB(255, 64, 64, 69),
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('Next->'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => SecondPage(),
+                  ),
+                );
+              },
+            )
+          ],
         ),
-        backgroundColor: Color.fromARGB(255, 182, 238, 203),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const CircleAvatar(
-                backgroundImage: AssetImage('images/image.png'),
-                radius: 155,
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetToKnowMe'),
+        backgroundColor: Color.fromARGB(255, 119, 179, 141),
+      ),
+      backgroundColor: Color.fromARGB(255, 182, 238, 203),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage('images/image.png'),
+              radius: 155,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              'Dawid Szosziaszwili',
+              style: GoogleFonts.lobster(
+                fontSize: 45,
+                color: const Color.fromARGB(255, 64, 64, 69),
               ),
-              const SizedBox(
-                height: 50,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Flutter Developer',
+              style: GoogleFonts.dancingScript(
+                fontSize: 40,
+                color: const Color.fromARGB(255, 64, 64, 69),
               ),
-              Text(
-                'Dawid Szosziaszwili',
-                style: GoogleFonts.lobster(
-                  fontSize: 45,
-                  color: const Color.fromARGB(255, 64, 64, 69),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Text(
-                'Flutter Developer',
-                style: GoogleFonts.dancingScript(
-                  fontSize: 40,
-                  color: const Color.fromARGB(255, 64, 64, 69),
-                ),
-              ),
-              ElevatedButton(
-                child: const Text('Next->'),
-                onPressed: () {},
-              )
-            ],
-          ),
+            ),
+            ElevatedButton(
+              child: const Text('<-Back'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => FirstPage(),
+                  ),
+                );
+              },
+            )
+          ],
         ),
       ),
     );
