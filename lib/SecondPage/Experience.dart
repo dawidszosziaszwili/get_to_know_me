d1ad4987e63d4e1741e603ca7d9cd6d7e0ca0518
Dawid Szosziaszwili <dawidszwili7@gmail.com>
2023-10-02 00:26:40 +0200

    add text color to experience

--- lib/SecondPage/Experience.dart
+++ lib/SecondPage/Experience.dart
@@ -19,22 +19,28 @@ class Experience extends StatelessWidget {
         children: [
           Text(
             '12/2022 - present - Account Manager in SquareOne Poland',
-            style: GoogleFonts.lato(fontSize: 25),
+            style: GoogleFonts.lato(
+                fontSize: 25, color: const Color.fromARGB(212, 44, 42, 189)),
           ),
           const SizedBox(
             height: 25,
           ),
           Text(
             '02/2022-12/2022 - IT Sales Specialist in Crestt Sp. z o.o.',
-            style: GoogleFonts.lato(fontSize: 25),
+            style: GoogleFonts.lato(fontSize: 25, color: Colors.black),
+          ),
+          const SizedBox(
+            height: 25,
           ),
           Text(
             '10/2021-02/2022 - Business Development Manager in Dasoft Sp. z o.o.',
-            style: GoogleFonts.lato(fontSize: 25),
+            style: GoogleFonts.lato(
+                fontSize: 25, color: const Color.fromARGB(255, 21, 26, 170)),
           ),
+          const SizedBox(height: 25),
           Text(
             '10/2022-02/2022 - Business Development Representative in Verizon Connect Sp. z o.o.',
-            style: GoogleFonts.lato(fontSize: 25),
+            style: GoogleFonts.lato(fontSize: 25, color: Colors.black),
           ),
           const CircleAvatar(
             backgroundImage: AssetImage('images/image6.png'),
